5ef0efac8e4ffebcdf8f3238f601cfeed0c56534
Hugo Chung <chunghugo99994@gmail.com>
2024-08-29 15:49:02 -0700

    Update user_schedule_model.py

--- user_schedule_model.py
+++ user_schedule_model.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-
 import os
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
@@ -26,7 +25,7 @@ class UserSchedule(Base):
     E2 = Column(String)
 
 # Connect to the Heroku PostgreSQL database
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['postgresql+psycopg2://u5hsl3t8vpl42s:pe6a13af81a75d26bf7ec16ed5614d296602e45c12f84e7dc965e840334951295@cd1goc44htrmfn.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d66o2tq3s18vlt']
 engine = create_engine(DATABASE_URL)
 
 # Create the tables in the database
