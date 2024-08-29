c4e9c15a272486b9f761a33ba7a1e2d2229e4067
Hugo Chung <chunghugo99994@gmail.com>
2024-08-29 16:19:17 -0700

    Update calenderBot.py

--- calenderBot.py
+++ calenderBot.py
@@ -11,7 +11,7 @@ from table2ascii import table2ascii as t2a, PresetStyle
 
 
 DATABASE_URL = os.environ['postgresql+psycopg2://u5hsl3t8vpl42s:pe6a13af81a75d26bf7ec16ed5614d296602e45c12f84e7dc965e840334951295@cd1goc44htrmfn.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d66o2tq3s18vlt']
-engine = create_engine(DATABASE_URL)
+engine = create_engine('postgresql+psycopg2://u5hsl3t8vpl42s:pe6a13af81a75d26bf7ec16ed5614d296602e45c12f84e7dc965e840334951295@cd1goc44htrmfn.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d66o2tq3s18vlt')
 Session = sessionmaker(bind=engine)
 session = Session()
 
