4eae522ce7fd119460e9552809783d6763abe45d
Hugo Chung <chunghugo99994@gmail.com>
2024-08-29 16:00:33 -0700

    Update user_schedule_model.py

--- user_schedule_model.py
+++ user_schedule_model.py
@@ -26,7 +26,7 @@ class UserSchedule(Base):
 
 # Connect to the Heroku PostgreSQL database
 DATABASE_URL = os.environ['postgresql//u5hsl3t8vpl42s:pe6a13af81a75d26bf7ec16ed5614d296602e45c12f84e7dc965e840334951295@cd1goc44htrmfn.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d66o2tq3s18vlt']
-engine = create_engine('postgresql://u5hsl3t8vpl42s:pe6a13af81a75d26bf7ec16ed5614d296602e45c12f84e7dc965e840334951295@cd1goc44htrmfn.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d66o2tq3s18vlt')
+engine = create_engine('postgresql+psycopg2://u5hsl3t8vpl42s:pe6a13af81a75d26bf7ec16ed5614d296602e45c12f84e7dc965e840334951295@cd1goc44htrmfn.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d66o2tq3s18vlt')
 
 # Create the tables in the database
 Base.metadata.create_all(engine)
