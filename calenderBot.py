fb787aefc61f074037b99a8e406eefdcfe7f09e7
Hugo Chung <chunghugo99994@gmail.com>
2024-10-16 09:04:26 -0700

    Update calenderBot.py

--- calenderBot.py
+++ calenderBot.py
@@ -1073,17 +1073,17 @@ async def send_reminders():
                 continue
             
             if user_pref.notification_method == 'DM':
-                print("Enters 3")
+                # print("Enters 3")
                 # print(current_time)
                 try: 
                     user_schedule = get_or_create_user_schedule(user_pref.discord_id)
                     user_courses = get_user_courses(user_schedule)
                     user_grade = int(user_schedule.grade)
                     if user_pref.notification_time.strftime("%H:%M") == current_time:
-                        print("Enters 4")
+                        # print("Enters 4")
                         user_reminders = get_reminders_for_user_on_date(user_pref.discord_id,tomorrow, user_courses, user_grade)
                         if user_reminders:
-                            print("Enters 5")
+                            # print("Enters 5")
                             await send_notification_dm(user_pref.discord_id, user_reminders)
                         else:
                             embed = discord.Embed(
